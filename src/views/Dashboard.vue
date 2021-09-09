495fe8854c336ec0eb17c65666b8f05d01a7108c
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-09-09 18:48:22 +0600

    dashboard added

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,281 @@
 <template>
-    <p>Dashboard</p>
+    <div>
+        <v-card rounded="0" elevation="0" color="#f2f5f8"> 
+            <v-breadcrumbs :items="items">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+            </v-breadcrumbs>
+        </v-card>
+        <v-row v-if="!userInfo" style="margin-top:20px;text-align:center;vertical-align:middle !important">
+            <v-col style="margin:auto">
+                <v-progress-circular
+                :size="70"
+                :width="7"
+                color="#d4d6d8"
+                indeterminate
+                ></v-progress-circular>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col lg="3" md="3" sm="12" cols="12">
+                <v-card  flat  class="ma-5" elevation="0">
+                    <h4>Dashboard</h4>
+                    <br>
+                    <v-row v-for="item in userInfo.dashboard" :key="item.text">
+                        <v-col>
+                           <v-card elevation="0" class="my-2" :color="getRandomColor()" link :to="'project/'+item.id">
+                               <v-row>
+                                   <v-col cols="3">
+                                       <v-icon class="mx-5 my-7" x-large>{{item.icon}}</v-icon>
+                                   </v-col>
+                                   <v-col cols="9">
+                                       <strong style="font-size:12;color:gray">{{item.text}}</strong> 
+                                       <br>
+                                       <h1 class="mt-4">{{item.count}}</h1>
+                                   </v-col>
+                               </v-row>
+                           </v-card>
+                        </v-col>
+                    </v-row>
+                </v-card>
+            </v-col>
+            <v-divider
+            class="mx-0"
+            vertical
+            ></v-divider>
+            <v-col lg="" md="9" sm="12" cols="12">
+                <v-card  class="ma-5" elevation="0">
+                   <v-row>
+                        <v-col>
+                            <v-card color="teal lighten-5" outlined class="mt-2 pa-4" elevation="0" style="border: 1px solid #e7e7e7" width="100%">
+                                <v-row>
+                                   <v-col cols="3">
+                                       <v-img sizes="10" class="ma-5 mx-15" style="width:150px;border:1px solid gray;border-radius:100px!important" src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                                   </v-col>
+                                   <v-col cols="4">
+                                       <h2 class="ma-4"><b>Dr. {{ userInfo.firstName }} {{ userInfo.lastname }}</b></h2>
+                                        <v-chip small outlined class="ml-4">{{userInfo.degree}} , {{userInfo.institute}}</v-chip>
+                                   </v-col>
+                                   <v-col>
+                                       <v-row>
+                                            <v-col>
+                                                <v-card-subtitle>
+                                                    <b>Email:</b> <br>{{userInfo.email}}
+                                                </v-card-subtitle>
+                                            </v-col>
+                                            <v-col align-self="center">
+                                                <v-chip color="teal" class="white--text" small link><b>{{userInfo.role}}</b></v-chip>
+                                            </v-col>
+                                       </v-row>
+                                       <v-row>
+                                            <v-col>
+                                                <v-card-subtitle>
+                                                    <b>Phone Number:</b> <br>{{userInfo.phone}}
+                                                </v-card-subtitle>
+                                            </v-col>
+                                            <v-col>
+                                                <v-card-subtitle>
+                                                    <b>Address:</b> <br> {{userInfo.address}}
+                                                </v-card-subtitle>
+                                            </v-col>
+                                       </v-row>
+                                   </v-col>
+                                </v-row>
+                            </v-card>
+                        </v-col>
+                   </v-row>
+                   <!-- appointment list  -->
+                                    <!-- member list  -->
+                   <v-row>
+                       <v-col>
+                           <v-card class="pa-4 mt-2" elevation="0" style="border: 1px solid #e7e7e7" width="100%">
+                               <v-row class="pa-5">
+                                   <v-icon large>mdi-clipboard-text</v-icon> <h3 class="mt-1 ml-2">Recent Appointment</h3>
+                               </v-row>
+                               <v-row style="background-color:#f2f5f8;border-radius:8px;text-align:center">
+                                   <v-col cols="4">
+                                       <b>Name</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Age</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Phone Number</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Address</b>
+                                   </v-col>
+                                   <v-col>
+                                       <b>Action</b>
+                                   </v-col>
+                               </v-row>
+                               <v-row v-for="appointment in appointmentList" :key="appointment.id" style="text-align:center;border-bottom: 1px solid #e7e7e7">
+                                    <v-col class="ml-2" style="text-align:left" cols="4">
+
+                                        <v-row>
+                                            <v-col cols="3">
+                                               <v-avatar
+                                               class="ma-3 white--text"
+                                                :color="getRandomColor()"
+                                                size="42"
+                                                ><h3>{{appointment.firstName.charAt(0)}}</h3></v-avatar>
+                                            </v-col>
+                                            <v-col>
+                                                 <h4 class="mt-5">
+                                                    {{appointment.firstName}} {{appointment.lastName}}
+                                                </h4>   
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                    <v-col>
+                                        <v-chip class="mt-3" small outlined color="teal">{{appointment.age}}</v-chip>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{appointment.phoneNo}}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                            {{appointment.address}}
+                                        </v-card-subtitle>
+                                    </v-col>
+                                    <v-col>
+                                        <v-card-subtitle>
+                                                <v-btn color="info" depressed small><v-icon small>mdi-prescription</v-icon></v-btn>
+                                        </v-card-subtitle>
+                                    </v-col>
+                               </v-row>
+                           </v-card>
+                       </v-col>
+                   </v-row>            
+                </v-card>
+            </v-col>
+        </v-row>
+
+    </div>
 </template>
+<script>
+import axios from "axios"
+export default {
+    data() {
+        return {
+            idx:0,
+            input: "",
+            adddialog: false,
+            GET_LOGGED_IN_PROFILE_API: "https://buggie-backend.herokuapp.com/auth/user-info",
+            user: {},
+            auth: "Bearer " + localStorage.getItem("token"),
+            userInfo: {
+                firstName:"Sandra",
+                lastname:"Adams",
+                institute:"Dhaka Medical College",
+                degree: "MBBS",
+                phone:"01734543027",
+                email:"sandra.dr@gmail.com",
+                role:"Doctor",
+                address: "Dhaka, Bangladesh",
+                dashboard: [
+                {
+                    text: "Todays Appointment",
+                    count: 80,
+                    icon: "mdi-playlist-minus"
+                },
+                {
+                    text: "Todays Earn",
+                    count: 800,
+                    icon: "mdi-cash"
+                },
+                {
+                    text: "Total Appointment",
+                    count: 320,
+                    icon: "mdi-playlist-check"
+                },
+                {
+                    text: "Total Earn",
+                    count: 34000,
+                    icon: "mdi-cash-multiple"
+                },
+            ]
+            },
+            appointmentList: [
+                {
+                    id:"1",
+                    firstName: "Injamamul Haque",
+                    lastName:"Sonet",
+                    age:"22",
+                    phoneNo: "017354635920",
+                    address:"Dhaka, Bangladesh"
+                },
+                {
+                    id:"2",
+                    firstName: "Faisul",
+                    lastName:"Islam",
+                    age:"19",
+                    phoneNo: "017354635920",
+                    address:"Dhaka, Bangladesh"
+                }
+            ],
+            items: [
+                {
+                text: 'a2sDMS',
+                disabled: false,
+                href: '/',
+                },
+                {
+                text: 'Dashboard',
+                disabled: true,
+                href: 'dashboard',
+                },
+            ],
+            
+        }
+    },
+    methods: {
+        getProfileInfo() {
+      console.log(this.auth)
+      axios({
+        method: "get",
+        url: this.GET_LOGGED_IN_PROFILE_API,
+        headers: {
+          Authorization: this.auth,
+          "Content-Type": "application/json"
+        }
+      })
+        .then(r => {
+        console.log(r.data)
+        this.userInfo = r.data;
+        localStorage.setItem("userInfo", JSON.stringify(r.data));
+        this.userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        console.log(this.userInfo.firstName)
+        // location.reload();
+                })
+        .catch(r => {
+            console.log(r)
+        });
+        },
+        getRandomColor() {
+        return 'rgb(' + 
+            (Math.floor(Math.random()*56)+200) + ', ' +
+            (Math.floor(Math.random()*56)+200) + ', ' +
+            (Math.floor(Math.random()*56)+200) +
+            ')';
+        },
+         show(){ 
+            return 0   
+        },
+        
+    },
+    mounted(){
+        // this.userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        // this.getProfileInfo();
+    },
+}
+</script>
+<style lang="scss" scoped>
+.rowise .col-6{
+    padding-bottom: 0px !important;
+    padding-top: 0px !important;
+}
+</style>
