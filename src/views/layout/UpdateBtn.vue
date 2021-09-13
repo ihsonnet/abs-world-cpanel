b16a56fa539258df37b574c2f09cc3658fafb579
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-09-14 02:20:29 +0600

    added update tootip

--- src/views/layout/UpdateBtn.vue
+++ src/views/layout/UpdateBtn.vue
@@ -1,4 +1,6 @@
 <template>
+    <v-tooltip left>
+     <template v-slot:activator="{ on, attrs }">
     <v-btn
       class="mx-2 cloud"
       fab
@@ -8,11 +10,16 @@
       right
       color="teal"
       elevation="2"
+      v-bind="attrs"
+      v-on="on"
     >
       <v-icon dark>
         mdi-cloud-upload
       </v-icon>
     </v-btn>
+    </template>
+    <span>Update From Server</span>
+    </v-tooltip>
 </template>
 <style scoped>
 .cloud {
